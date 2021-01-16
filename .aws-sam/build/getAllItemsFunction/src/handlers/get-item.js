130311e42a888ed53cc11db1936fa894c701f50f
Steven Castro <castrosteven>
2021-01-16 18:35:42 -0500

    search by id

--- .aws-sam/build/getAllItemsFunction/src/handlers/get-item.js
+++ .aws-sam/build/getAllItemsFunction/src/handlers/get-item.js
@@ -1,13 +1,14 @@
 const tableName = process.env.SAMPLE_TABLE;
 
 const dynamodb = require("aws-sdk/clients/dynamodb");
-const docClient = new dynamodb.DocumentClient();
+const docClient = new dynamodb.DocumentClient({ apiVersion: "2012-08-10" });
 
 exports.getItemHandler = async (event) => {
   if (event.httpMethod !== "GET") {
     throw new Error(`MUST BE A GET: ${event.httpMethod}`);
   }
   console.info("received:", event);
+
   var params = {
     TableName: tableName,
     Key: {
@@ -16,11 +17,10 @@ exports.getItemHandler = async (event) => {
   };
 
   const data = await docClient.get(params).promise();
-  const items = data.Items;
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(items),
+    body: JSON.stringify(data.Item),
   };
 
   return response;
